e49ad6c6b101f95f9288fc507798bacab5d5397a
Yingquan Yuan <yingq.yuan@gmail.com>
2014-03-03 19:36:13 -0500

    Updates grep script to support more languages

--- lines.sh
+++ lines.sh
@@ -1,42 +1,31 @@
-#!/bin/sh
-# temporarily supported language
-java="java"
-c="c"
-h="h"
-cpp="cpp"
-js="js"
-css="css"
-jsp="jsp"
-racket="rkt"
-sql="sql"
-xml="xml"
-# temporarily supported language
+#!/bin/bash
+
+# supported language
+suffices=("java" "c" "h" "cpp" "js" "css" "jsp" "rkt" "sql" "xml" "py" "rb" "sh")
+pattern='^\s*\/\*|^\s*\*|^\s*\/\/|^\s*;|^\s*--|^\s*#'
 
 if [ $# -lt 1 ]; then
 cat<<HELP
-	lines -- count the number of lines of uncommented codes in a sequence of source files.
+    lines -- count the number of lines of uncommented codes in a sequence of source files.
 
-	USAGE: ./lines file1 file2 ... filen or ./lines regexp
+    USAGE: ./lines file1 file2 ... filen or ./lines wildcards
 HELP
-	exit 0
+    exit 0
 fi
+
 total=0
-echo "Number of uncommented lines of codes, with blank lines trimed:"
+
 for file in $*; do
-	if [ -f "$file" ]; then
-		suffix="${file##*.}"
-		if [ "$suffix" = "$java" -o "$suffix" = "$c" -o "$suffix" = "$h" -o "$suffix" = "$cpp" -o "$suffix" = "$js" -o "$suffix" = "$css" -o "$suffix" = "$jsp" -o "$suffix" = "$racket" -o "$suffix" = "$sql" -o "$suffix" = "$xml" ]; then
-			filename="$file"
-			result=`grep -Ev '^\s{0,}\/\*|^\s{0,}\*|^\s{0,}\/\/|^\s{0,};|^--' ${filename} | grep -c '[[:graph:]]' -`
-			total=`expr $total + $result`
-			echo "$filename: $result"
-		else
-			echo "$file: unsupported file, not a source code file"
-		fi
-	fi
+    if [ -f "$file" ]; then
+        suffix="${file##*.}"
+        if [[ ${suffices[@]} =~ $suffix ]]; then
+            count=`grep -Ev $pattern ${file} | grep -c '[[:graph:]]' -`
+            echo "$file: $count"
+            total=`expr $total + $count`
+        else
+            echo "$file: unsupported file type .$suffix"
+        fi
+    fi
 done
+
 echo "Total: $total"
-
-# problematic '^\/\*|^[[:space:]]\*|^(+[[:space:]])\/\*|^\/\/'
-# good '^\s{0,}\/\*|^\s{0,}\*|^\s{0,}\/\/'
-
